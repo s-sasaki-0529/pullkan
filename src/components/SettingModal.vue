99ac9e337d0dc7ce3529362e36da25dffa76a433
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-09-20 13:40:58 +0900

    設定モーダルにマルチセレクタUIを表示

--- src/components/SettingModal.vue
+++ src/components/SettingModal.vue
@@ -3,11 +3,29 @@
     <div class="modal-background" />
     <div class="modal-card">
       <header class="modal-card-head">
-        <p class="modal-card-title">Modal title</p>
+        <p class="modal-card-title">Settings</p>
         <button class="delete" aria-label="close" @click="props.onClose" />
       </header>
       <section class="modal-card-body">
-        準備中...
+        <form>
+          <label class="label">Repositories</label>
+          <div class="select-repository select is-multiple">
+            <select multiple size="8">
+              <option value="Argentina">Argentina</option>
+              <option value="Bolivia">Bolivia</option>
+              <option value="Brazil">Brazil</option>
+              <option value="Chile">Chile</option>
+              <option value="Colombia">Colombia</option>
+              <option value="Ecuador">Ecuador</option>
+              <option value="Guyana">Guyana</option>
+              <option value="Paraguay">Paraguay</option>
+              <option value="Peru">Peru</option>
+              <option value="Suriname">Suriname</option>
+              <option value="Uruguay">Uruguay</option>
+              <option value="Venezuela">Venezuela</option>
+            </select>
+          </div>
+        </form>
       </section>
       <footer class="modal-card-foot">
         <button class="button is-success" @click="save">Save</button>
@@ -39,3 +57,12 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" scoped>
+.select-repository {
+  width: 100%;
+  select {
+    width: 100%;
+  }
+}
+</style>
