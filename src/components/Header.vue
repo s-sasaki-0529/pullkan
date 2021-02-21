ea9a6e77ef65e66e60651f75ff8b679340a96c5e
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-02-21 22:05:19 +0900

    ヘッダーに自身のユーザー情報を表示するように変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,9 @@
 <template>
   <header class="header">
-    <h1 class="repository">
-      <a href="https://github.com/StudistCorporation" target="_blank">
-        StudistCorporation
-      </a>
-      /
-      <a href="https://github.com/StudistCorporation/teachme_web_duvel" target="_blank">
-        teachme_web_duvel
-      </a>
-    </h1>
+    <template v-if="currentUser.name">
+      <img class="current-user-icon" :src="currentUser.avatarUrl" />
+      <a :href="`https://github.com/${currentUser.name}`" class="current-user-name">{{ currentUser.name }}</a>
+    </template>
     <span class="icon" :class="{ loading: store.state.onLoading }" @click="reload">
       <i class="fas fa-sync"></i>
     </span>
@@ -21,7 +16,7 @@
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from 'vue'
+import { ref, defineComponent, computed } from 'vue'
 import { useStore } from '@/composition/store'
 import { useSetting } from '@/composition/setting'
 import SettingModal from '@/components/SettingModal.vue'
@@ -37,12 +32,13 @@ export default defineComponent({
 
     const reload = () => store.reload(setting.state)
     const showConfigModal = () => (isShowSettingModal.value = true)
-
+    const currentUser = computed(() => store.state.currentUser)
     return {
       store,
+      currentUser,
       isShowSettingModal,
-      reload,
-      showConfigModal
+      showConfigModal,
+      reload
     }
   }
 })
@@ -51,11 +47,18 @@ export default defineComponent({
 <style lang="scss" scoped>
 .header {
   display: flex;
+  justify-content: flex-end;
   align-items: center;
   padding: 30px;
   height: 50px;
-  .repository {
-    font-size: 1.75em;
+  .current-user-icon {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+  .current-user-name {
+    font-size: 1.5em;
   }
   .icon {
     margin-left: 5px;
