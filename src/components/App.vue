dae41b3a4a738831de8ef177e32cdf567c6fe965
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-04-04 17:25:27 +0900

    同じコンポーネントで provide と inject を同時にしないように

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,61 +1,21 @@
 <template>
-  <div class="app" v-if="state.authState === 'success'">
-    <Header />
-    <Board />
-  </div>
-  <div class="on-error" v-else-if="state.authState === 'error'">
-    <button @click="signIn" class="button is-rounded">
-      <span class="icon">
-        <i class="fab fa-github"></i>
-      </span>
-      <span>Sign in with GitHub</span>
-    </button>
-  </div>
+  <Pullkan />
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, watch } from 'vue'
-import { tryAuth, signInWithGitHub } from '@/lib/authentication'
-import { provideStore, useStore } from '@/composition/store'
-import { provideSetting, useSetting } from '@/composition/setting'
-import Header from './Header.vue'
-import Board from './Board.vue'
+import { defineComponent } from 'vue'
+import { provideStore } from '@/composition/store'
+import { provideSetting } from '@/composition/setting'
+import Pullkan from './Pullkan.vue'
 
 export default defineComponent({
   components: {
-    Header,
-    Board
+    Pullkan
   },
-
   setup() {
     provideStore()
     provideSetting()
-
-    const state = reactive<{ authState: 'success' | 'error' | 'doing' }>({ authState: 'doing' })
-    const store = useStore()
-    const setting = useSetting()
-
-    watch(
-      () => state.authState,
-      newStatus => {
-        if (newStatus === 'success') {
-          setting.load()
-          store.reload(setting.state)
-        }
-      }
-    )
-
-    tryAuth()
-      .then(() => (state.authState = 'success'))
-      .catch(() => (state.authState = 'error'))
-
-    const signIn = () => {
-      signInWithGitHub().then(() => {
-        location.href = '/'
-      })
-    }
-
-    return { state, signIn }
+    return {}
   }
 })
 </script>
---
+++ src/components/Pullkan.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="pullkan" v-if="state.authState === 'success'">
+    <Header />
+    <Board />
+  </div>
+  <div class="on-error" v-else-if="state.authState === 'error'">
+    <button @click="signIn" class="button is-rounded">
+      <span class="icon">
+        <i class="fab fa-github"></i>
+      </span>
+      <span>Sign in with GitHub</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, watch } from 'vue'
+import { tryAuth, signInWithGitHub } from '@/lib/authentication'
+import { useStore } from '@/composition/store'
+import { useSetting } from '@/composition/setting'
+import Header from './Header.vue'
+import Board from './Board.vue'
+
+export default defineComponent({
+  components: {
+    Header,
+    Board
+  },
+
+  setup() {
+    const state = reactive<{ authState: 'success' | 'error' | 'doing' }>({ authState: 'doing' })
+    const store = useStore()
+    const setting = useSetting()
+
+    watch(
+      () => state.authState,
+      newStatus => {
+        if (newStatus === 'success') {
+          setting.load()
+          store.reload(setting.state)
+        }
+      }
+    )
+
+    tryAuth()
+      .then(() => (state.authState = 'success'))
+      .catch(() => (state.authState = 'error'))
+
+    const signIn = () => {
+      signInWithGitHub().then(() => {
+        location.href = '/'
+      })
+    }
+
+    return { state, signIn }
+  }
+})
+</script>
+
+<style lang="scss">
+@import '../../node_modules/bulma/bulma.sass';
+
+.pullkan {
+  height: 100vh;
+  .g-full-height {
+    height: 100%;
+  }
+}
+</style>
